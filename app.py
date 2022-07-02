3eda0bef884b6f18f5c7c4c10a08b5059e0d7c80
RodrigoOlman <58253035+RodrigoOlman@users.noreply.github.com>
2022-07-02 01:56:10 +0100

    changed name to app

---
+++ app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from flask import Flask
+from flask import jsonify
+from flask import request
+#from dataframe import df
+
+#weekly data of covid cases
+url = "https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/data.csv"
+
+#initial 
+df = pd.read_csv(url)
+
+
+#instance of flask class
+app = Flask(__name__)
+
+## the route decorator will tell flask what url should trigger the function, and the allowed http methods
+@app.route("/", methods =["GET","POST"])
+def welcome():
+    return "hello tiago!"
+
+#print(df)
+
+@app.route("/data/")
+def hello2():
+    return jsonify(df.to_json(orient ='index'))
+
+if __name__ == "__main__":
+    #runs the flask aplication
+    #host specifies the server we want our aplication to run
+    app.run()
