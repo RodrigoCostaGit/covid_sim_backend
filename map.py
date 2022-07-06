07a905882222fca8acc54424e9e737c9702c9ebc
RodrigoOlman <58253035+RodrigoOlman@users.noreply.github.com>
2022-07-06 19:01:14 +0100

    added info to data region

--- map.py
+++ map.py
@@ -11,5 +11,10 @@ df = df.iloc [:,3:10]
 df = df[["confirmados_arsalentejo","confirmados_arsalgarve","confirmados_arslvt","confirmados_arscentro","confirmados_arsnorte","confirmados_madeira","confirmados_acores"]]
 df2 = df.rename({"confirmados_arsalentejo":"Alentejo","confirmados_arsalgarve":"Algarve","confirmados_arslvt":"Área Metropolitana de Lisboa","confirmados_arscentro":"Centro (PT)","confirmados_arsnorte":"Norte","confirmados_madeira":"Região Autónoma da Madeira","confirmados_acores":"Região Autónoma dos Açores"}, axis=1)
 df2 = df2.tail(1)
+index = df2.head().index.values
+index = index[0]
+df2 = df2.rename(index={index:"info"})
 df2 = df2.T
+print(df2)
+
 df2.to_csv("data_regions.csv")
