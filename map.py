2a988c429b4f9e8c03a4074b5fc8211726270bd9
RodrigoOlman <58253035+RodrigoOlman@users.noreply.github.com>
2022-07-06 17:07:53 +0100

    fixed data regions and added map

---
+++ map.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+#weekly data of covid cases
+url = "https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/data.csv"
+
+#loads the csv to df
+df = pd.read_csv(url)
+
+
+df = df.iloc [:,3:10]
+df = df[["confirmados_arsalentejo","confirmados_arsalgarve","confirmados_arslvt","confirmados_arscentro","confirmados_arsnorte","confirmados_madeira","confirmados_acores"]]
+df2 = df.rename({"confirmados_arsalentejo":"Alentejo","confirmados_arsalgarve":"Algarve","confirmados_arslvt":"Área Metropolitana de Lisboa","confirmados_arscentro":"Centro (PT)","confirmados_arsnorte":"Norte","confirmados_madeira":"Região Autónoma da Madeira","confirmados_acores":"Região Autónoma dos Açores"}, axis=1)
+df2 = df2.tail(1)
+df2 = df2.T
+df2.to_csv("data_regions.csv")
